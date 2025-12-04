b0a0bf85b21c0fd68386d0a84b4bc7eda4f25d7b
Meypod <radical.zero@hotmail.com>
2025-12-04 17:46:02 +0330

    feat: minimal dagger hilt setup

--- app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
+++ app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.github.meypod.al_azan
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,9 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.meypod.al_azan.core.presentation.AlAzanTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
