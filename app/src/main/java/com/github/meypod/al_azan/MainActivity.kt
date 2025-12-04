95a238e789cc5d7db0ddf775fb8185e71d63a0ae
Meypod <radical.zero@hotmail.com>
2025-12-04 05:03:42 +0330

    fix: update theme colors

--- app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
+++ app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.meypod.al_azan.ui.theme.AlAzanTheme
+import com.github.meypod.al_azan.core.presentation.AlAzanTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/github/meypod/al_azan/core/presentation/Color.kt
@@ -0,0 +1,10 @@
+package com.github.meypod.al_azan.core.presentation
+
+import androidx.compose.ui.graphics.Color
+
+val Primary40 = Color(0xFF00585A)
+val Primary80 = Color(0xFF76D5D8)
+val Secondary40 = Color(0xFF436465)
+val Secondary80 = Color(0xFFAACDCE)
+val Tertiary40 = Color(0xFF705D00)
+val Tertiary80 = Color(0xFFFFE487)
