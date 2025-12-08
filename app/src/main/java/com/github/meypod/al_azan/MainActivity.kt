cbbc1bdfdfd25c945c9b50695278b9d92eb71500
Meypod <radical.zero@hotmail.com>
2025-12-08 17:58:20 +0330

    fix: bare minimum nav3 impl

--- app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
+++ app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
@@ -1,51 +1,31 @@
 package com.github.meypod.al_azan
 
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.meypod.al_azan.core.presentation.AlAzanTheme
+import com.github.meypod.al_azan.core.presentation.navigation.NavigationRoot
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AlAzanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      AlAzanTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+          NavigationRoot(modifier = Modifier.padding(paddingValues))
         }
+      }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlAzanTheme {
-        Greeting("Android")
-    }
+  }
 }
---
+++ app/src/main/java/com/github/meypod/al_azan/intro/IntroNavigation.kt
@@ -0,0 +1,51 @@
+package com.github.meypod.al_azan.intro
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
+import androidx.navigation3.ui.NavDisplay
+import androidx.savedstate.serialization.SavedStateConfiguration
+import com.github.meypod.al_azan.core.presentation.navigation.Route
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+@Composable
+fun IntroNavigation(modifier: Modifier = Modifier) {
+  val introBackstack =
+      rememberNavBackStack(
+          configuration =
+              SavedStateConfiguration {
+                serializersModule = SerializersModule {
+                  polymorphic(NavKey::class) {
+                    subclass(
+                        Route.Intro.LanguageSelection::class,
+                        Route.Intro.LanguageSelection.serializer(),
+                    )
+                  }
+                }
+              },
+          Route.Intro.LanguageSelection,
+      )
+
+  NavDisplay(
+      backStack = introBackstack,
+      modifier = modifier,
+      entryDecorators =
+          listOf(
+              rememberSaveableStateHolderNavEntryDecorator(),
+              rememberViewModelStoreNavEntryDecorator(),
+          ),
+      entryProvider =
+          entryProvider {
+            entry<Route.Intro.LanguageSelection> {
+              // todo
+              Text("Intro")
+            }
+          },
+  )
+}
