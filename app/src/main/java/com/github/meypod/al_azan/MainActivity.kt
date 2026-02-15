78b805d47bf82f5c35726634c669d4bf6421371d
Meypod <radical.zero@hotmail.com>
2026-02-15 13:47:07 +0330

    chore: directly init intro if intro is not done

--- app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
+++ app/src/main/java/com/github/meypod/al_azan/MainActivity.kt
@@ -9,21 +9,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.github.meypod.al_azan.core.domain.repository.SettingsRepository
 import com.github.meypod.al_azan.core.presentation.AlAzanTheme
 import com.github.meypod.al_azan.core.presentation.navigation.NavigationRoot
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var settingsRepository: SettingsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val appIntroDone = runBlocking { settingsRepository.fetch().appIntroDone }
         setContent {
             AlAzanTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
-                    NavigationRoot(modifier = Modifier.padding(paddingValues))
+                    NavigationRoot(modifier = Modifier.padding(paddingValues), appIntroDone)
                 }
             }
         }
