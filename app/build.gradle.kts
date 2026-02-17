f685073161f62a0602414b57241577cd0cbc9ccf
Meypod <radical.zero@hotmail.com>
2026-02-17 07:14:44 +0330

    fix: add .debug to debug builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
