bf75a2d548188ef5ce7c694459bda12ea927ff34
Meypod <radical.zero@hotmail.com>
2026-02-21 22:08:56 +0330

    fix: truncate inside text if not fits

--- app/src/main/java/com/github/meypod/al_azan/core/presentation/components/CompactOutlinedTextField.kt
+++ app/src/main/java/com/github/meypod/al_azan/core/presentation/components/CompactOutlinedTextField.kt
@@ -1,23 +1,30 @@
 package com.github.meypod.al_azan.core.presentation.components
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.meypod.al_azan.core.presentation.AlAzanTheme
@@ -37,6 +44,7 @@ fun CompactOutlinedTextField(
     trailingIcon: (@Composable () -> Unit)? = null,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
 
     BasicTextField(
         value = value,
@@ -54,7 +62,19 @@ fun CompactOutlinedTextField(
     ) { innerTextField ->
         OutlinedTextFieldDefaults.DecorationBox(
             value = value,
-            innerTextField = innerTextField,
+            innerTextField = {
+                if (readOnly || !isFocused) {
+                    Text(
+                        text = value,
+                        modifier = Modifier,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = textStyle.merge(TextStyle(color = colors.unfocusedTextColor)),
+                    )
+                } else {
+                    innerTextField()
+                }
+            },
             enabled = enabled,
             singleLine = true,
             visualTransformation = VisualTransformation.None,
@@ -64,7 +84,6 @@ fun CompactOutlinedTextField(
             placeholder = {
                 BasicText(
                     placeholder,
-                    modifier = Modifier.fillMaxWidth(),
                     style = textStyle.merge(TextStyle(color = MaterialTheme.colorScheme.onSurfaceVariant)),
                 )
             },
