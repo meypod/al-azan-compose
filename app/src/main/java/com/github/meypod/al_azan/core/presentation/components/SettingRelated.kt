ebe2fda1168b0b8c48e12c26455b58b5cf6eb936
Meypod <radical.zero@hotmail.com>
2026-03-03 04:20:30 +0330

    fix: make a component for setting switches

--- app/src/main/java/com/github/meypod/al_azan/core/presentation/components/SettingRelated.kt
+++ app/src/main/java/com/github/meypod/al_azan/core/presentation/components/SettingRelated.kt
@@ -1,24 +1,33 @@
 package com.github.meypod.al_azan.core.presentation.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.github.meypod.al_azan.R
 import com.github.meypod.al_azan.core.presentation.AlAzanTheme
+import com.github.meypod.al_azan.main.location.LocationUiAction
 
 @Composable
 fun RowScope.SettingLabel(
@@ -55,6 +64,35 @@ fun SettingHeader(
     Column {
         SettingLabel(title)
         SettingHelp(subtitle)
+    }
+}
+
+@Composable
+fun SettingSwitch(
+    title: String,
+    subtitle: String?,
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Row(
+        modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Column(Modifier.weight(1f)) {
+            Text(title)
+            subtitle?.let {
+                Text(
+                    it,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
+            }
+        }
+        Spacer(Modifier.padding(start = dimensionResource(R.dimen.element_padding)))
+        Switch(checked, onCheckedChange)
     }
 }
 
@@ -101,6 +139,18 @@ private fun SettingHeaderPreview() {
         ACard {
             Column(Modifier.padding(it)) {
                 SettingHeader("This is a title", "this is a subtitle")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SettingSwitchPreview() {
+    AlAzanTheme {
+        ACard {
+            Column(Modifier.padding(it)) {
+                SettingSwitch("This is a title", "this is a subtitle", checked = false) {}
             }
         }
     }
