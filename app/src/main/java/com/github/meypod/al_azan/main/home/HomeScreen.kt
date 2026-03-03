e68f1dac59fbdd68f7e7d4037c3b0f65bf028bf3
Meypod <radical.zero@hotmail.com>
2026-03-03 17:18:15 +0330

    feat: add navigation drawer and items

--- app/src/main/java/com/github/meypod/al_azan/main/home/HomeScreen.kt
+++ app/src/main/java/com/github/meypod/al_azan/main/home/HomeScreen.kt
@@ -6,12 +6,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -21,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.meypod.al_azan.R
 import com.github.meypod.al_azan.core.presentation.AlAzanTheme
+import kotlinx.coroutines.launch
 import java.util.Date
 import kotlin.time.Instant
 import kotlin.time.toJavaInstant
@@ -32,29 +40,93 @@ fun HomeScreen(
     onAction: (HomeUiAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = {
-                        onAction(HomeUiAction.OnMenuIconClick)
-                    }) {
-                        Icon(painterResource(R.drawable.menu), contentDescription = stringResource(R.string.menu))
-                    }
-                },
-                title = {
-                    Text(formatInstant(uiState.currentInstant, uiState.locale, uiState.calendar))
-                },
-            )
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(drawerState) {
+                Text(stringResource(R.string.app_name), modifier = Modifier.padding(dimensionResource(R.dimen.page_padding)))
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(painterResource(R.drawable.alarm), contentDescription = null)
+                    },
+                    label = { Text(stringResource(R.string.reminder)) },
+                    selected = false,
+                    onClick = {
+                        onAction(HomeUiAction.OnReminderLinkClick)
+                    },
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(painterResource(R.drawable.compass_outline), contentDescription = null)
+                    },
+                    label = { Text(stringResource(R.string.qibla)) },
+                    selected = false,
+                    onClick = {
+                        onAction(HomeUiAction.OnQiblaLinkClick)
+                    },
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(painterResource(R.drawable.counter), contentDescription = null)
+                    },
+                    label = { Text(stringResource(R.string.counter)) },
+                    selected = false,
+                    onClick = {
+                        onAction(HomeUiAction.OnCounterLinkClick)
+                    },
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(painterResource(R.drawable.settings), contentDescription = null)
+                    },
+                    label = { Text(stringResource(R.string.settings)) },
+                    selected = false,
+                    onClick = {
+                        onAction(HomeUiAction.OnSettingsLinkClick)
+                    },
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(painterResource(R.drawable.info_variant_outline), contentDescription = null)
+                    },
+                    label = { Text(stringResource(R.string.about_us)) },
+                    selected = false,
+                    onClick = {
+                        onAction(HomeUiAction.OnAboutUsLinkClick)
+                    },
+                )
+            }
         },
-    ) { paddingValues ->
-        Column(
-            modifier
-                .padding(paddingValues)
-                .padding(dimensionResource(R.dimen.page_padding)),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.element_padding)),
-        ) {
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(painterResource(R.drawable.menu), contentDescription = stringResource(R.string.menu))
+                        }
+                    },
+                    title = {
+                        Text(formatInstant(uiState.currentInstant, uiState.locale, uiState.calendar))
+                    },
+                )
+            },
+        ) { paddingValues ->
+            Column(
+                modifier
+                    .padding(paddingValues)
+                    .padding(dimensionResource(R.dimen.page_padding)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.element_padding)),
+            ) {
+            }
         }
     }
 }
