82e6704334bbf8f073b19d6702932ab592ec2230
Meypod <radical.zero@hotmail.com>
2026-03-08 19:22:35 +0330

    fix: improve UiText

--- app/src/main/java/com/github/meypod/al_azan/core/presentation/UiText.kt
+++ app/src/main/java/com/github/meypod/al_azan/core/presentation/UiText.kt
@@ -2,22 +2,31 @@ package com.github.meypod.al_azan.core.presentation
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.res.stringResource
 
 sealed interface UiText {
+
+    @Immutable
     data class DynamicString(
         val value: String,
     ) : UiText
 
+    @Immutable
     class StringResourceId(
         @param:StringRes val id: Int,
-        val args: Array<Any> = arrayOf(),
+        vararg val formatArgs: Any = arrayOf(),
     ) : UiText
 
     @Composable
     fun asString(): String =
         when (this) {
             is DynamicString -> value
-            is StringResourceId -> stringResource(id, args)
+
+            is StringResourceId -> if (formatArgs.isNotEmpty()) {
+                stringResource(id, *formatArgs)
+            } else {
+                stringResource(id)
+            }
         }
 }
