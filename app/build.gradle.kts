9b35b9267b819b9caf6f7928f15524fefc7a7db9
Meypod <radical.zero@hotmail.com>
2026-04-17 21:55:21 +0800

    fix: migrate to AGP 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.dagger.hilt)
@@ -16,7 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.github.meypod.al_azan"
         minSdk = 26
-        targetSdk = 36
+        compileSdk = 37
+        targetSdk = 37
         versionCode = 78
         versionName = "2.0.0"
 
@@ -36,11 +36,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
 
     buildFeatures {
         compose = true
@@ -48,6 +43,12 @@ android {
     }
 
     androidResources { generateLocaleConfig = true }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
 }
 
 dependencies {
