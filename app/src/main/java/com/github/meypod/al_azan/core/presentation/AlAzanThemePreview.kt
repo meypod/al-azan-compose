ff6db57dfd33a2d7c41384ba4dfb9a37a737162f
Meypod <radical.zero@hotmail.com>
2026-04-29 10:19:53 +0700

    feat: implement WeekdayChipRow & edit compact padding & fix preview

--- app/src/main/java/com/github/meypod/al_azan/core/presentation/AlAzanThemePreview.kt
+++ app/src/main/java/com/github/meypod/al_azan/core/presentation/AlAzanThemePreview.kt
@@ -3,7 +3,10 @@ package com.github.meypod.al_azan.core.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,7 +16,7 @@ import com.github.meypod.al_azan.R
 @Composable
 fun AlAzanThemePreview(content: @Composable () -> Unit) {
     AlAzanTheme {
-        Surface {
+        Surface(Modifier.windowInsetsPadding(WindowInsets.safeDrawing)) {
             content()
         }
     }
@@ -27,7 +30,9 @@ fun AlAzanThemePaddedPreview(
     AlAzanTheme {
         Surface {
             Column(
-                modifier = modifier.padding(dimensionResource(R.dimen.page_padding)),
+                modifier = modifier
+                    .windowInsetsPadding(WindowInsets.safeDrawing)
+                    .padding(dimensionResource(R.dimen.page_padding)),
                 verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.element_padding)),
                 content = content,
             )
