a0455efcb22716aeb1011c89b331653e5e932dc3
Meypod <radical.zero@hotmail.com>
2026-06-10 12:40:07 +0330

    fix(reminder): add navigation bars padding

--- app/src/main/java/com/github/meypod/al_azan/main/reminder/ReminderScreen.kt
+++ app/src/main/java/com/github/meypod/al_azan/main/reminder/ReminderScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
@@ -118,6 +119,7 @@ fun ReminderScreen(
                 Column(
                     Modifier
                         .fillMaxWidth()
+                        .navigationBarsPadding()
                         .padding(dimensionResource(R.dimen.page_padding)),
                     verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.element_padding)),
                 ) {
