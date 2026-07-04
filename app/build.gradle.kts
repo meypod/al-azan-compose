5d8a64293ebd3dfa7e2579e0ea5e13841b31d18a
Meypod <radical.zero@hotmail.com>
2026-07-04 18:50:45 +0330

    chore: bump dependencies and add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 26
         compileSdk = 37
         targetSdk = 36
-        versionCode = 85
-        versionName = "2.0.6"
+        versionCode = 86
+        versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
