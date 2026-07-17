29643c91d0493104dd30bd26ad9a0da35a8b4df2
Meypod <radical.zero@hotmail.com>
2026-07-17 18:05:38 +0330

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 26
         compileSdk = 37
         targetSdk = 36
-        versionCode = 90
-        versionName = "2.1.1"
+        versionCode = 91
+        versionName = "2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
